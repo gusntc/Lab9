f2308c7d5ef7b410ab01e26c05bcc57e35b8066f
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-07 15:45:09 +0700

    Lab9_3Edit

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,3 +9,41 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int age,hight,prop;
+int main(){
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> hight;
+        if(hight < 160){
+            cout << "Your status = " << "UNFRIEND" ;
+        }else if(hight < 175){
+            cout << "Your status = " << "FRIEND" ;
+        }else{
+            cout << "Enter your property: ";
+            cin >> prop;
+            if(prop > 69e6){
+                cout << "Your status = " << "MARRIED" ;
+            }else{
+                cout << "Your status = " << "ONE-NIGHT-STAND" ;
+            }
+        }
+    }else if(age < 30){
+        cout << "Enter your property: ";
+        cin >> prop;
+        if(prop > 10e6){
+            cout << "Your status = " << "BEST FRIEND";
+        }else{
+            cout << "Your status = " << "UNFRIEND";
+        }
+    }else{
+        cout << "Your status = " << "UNFRIEND";
+    }
+    
+}
