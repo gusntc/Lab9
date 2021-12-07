97821007b2c849ae1dac75da23519c69ac5e42d4
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-07 15:57:40 +0700

    Lab9_2Edit

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char A,B,C,D,F;
+char findGrade(double x){
+	return (x > 90 ? 'A' : x > 75 ? 'B' : x > 60 ? 'C' : x > 45 ? 'D' : 'F' );
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +22,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
